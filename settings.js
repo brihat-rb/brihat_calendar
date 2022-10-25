34e46174fee0383a2b5df3adb4671d695975b474
brihat-rb <brihat.rb@gmail.com>
2022-10-25 15:42:44 +0545

    added console info, sweet alert and other changes

--- settings.js
+++ settings.js
@@ -19,6 +19,7 @@ function toggle_info_box() {
     else {
         document.getElementById('parvas').style.display = "none";
     }
+    console.info("Preferences (Show Event Info Box) set to: ", check_info_box.checked);
 }
 
 function toggle_date_jumper() {
@@ -30,6 +31,7 @@ function toggle_date_jumper() {
     else {
         document.getElementById('brihat_calendar_date_jumper').style.display = "none";
     }
+    console.info("Preferences (Show Date Jumper) set to: ", check_date_jumper.checked);
 }
 
 function toggle_tithis() {
@@ -47,6 +49,7 @@ function toggle_tithis() {
             lunar_infos[i].style.display = "none";
         }
     }
+    console.info("Preferences (Show Tithis) set to: ", check_tithis.checked);
 }
 
 function toggle_converter_link() {
@@ -58,6 +61,7 @@ function toggle_converter_link() {
     else {
         document.getElementById('date_converter_div').style.display = "none";
     }
+    console.info("Preferences (Show Date Converter Link) set to: ", check_converter_link.checked);
 }
 
 function toggle_print_button() {
@@ -69,6 +73,7 @@ function toggle_print_button() {
     else {
         document.getElementById('print').style.display = "none";
     }
+    console.info("Preferences (Show Print Button) set to: ", check_print_button.checked);
 }
 
 function toggle_invert_color() {
@@ -80,6 +85,7 @@ function toggle_invert_color() {
     else {
         document.getElementsByTagName('html')[0].style.filter = "none";
     }
+    console.info("Preferences (Invert Color) set to: ", invert_color.checked);
 }
 
 function trigger_all_event() {
@@ -91,7 +97,8 @@ function trigger_all_event() {
     toggle_invert_color();
 }
 
-var config = [true, false, false, false, true, false];
+var config = [];
+const default_config = [true, false, false, false, true, false];
 
 
 function save_current_config() {
@@ -102,13 +109,16 @@ function save_current_config() {
     config[4] = document.getElementById('check_date_jumper').checked;
     config[5] = document.getElementById('invert_color').checked;
     localStorage.setItem("config", JSON.stringify(config));
+    console.info("Current Preferences Saved Successfully.");
+    swal("Success!", "Current Config Saved", "success");
 }
 
 function load_config(def = false) {
     config = JSON.parse(localStorage.getItem("config"));
     if(config == null || def == true) {
-        config = [true, false, false, false, true, false];
-        save_current_config();
+        localStorage.setItem("config", JSON.stringify(default_config));
+        console.info("No User Preferences Found: Default Loaded and Saved.");
+        load_config();
     }
     else {
         document.getElementById('check_tithis').checked = config[0];
@@ -118,11 +128,33 @@ function load_config(def = false) {
         document.getElementById('check_date_jumper').checked = config[4];
         document.getElementById('invert_color').checked = config[5];
         trigger_all_event();
+        console.info("User Preferences Loaded Successfully.");
     }
 }
 
 function reset_default_config() {
-    load_config(true);
+    swal({
+        title: "Are you sure?",
+        text: "Restore Default Config?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            load_config(true);
+            console.info("Default User Preferences Loaded Successfully.");
+            swal("Success!", "Default Config Restored", "success", {icon: "success"});
+        }
+        else {
+          swal("Aborted");
+          console.info("Preferences Reset Operation Cancelled");
+        }
+      });
 }
 
 load_config();
+
+document.getElementById("setting_pane_author").innerHTML = "<i class='fa fa-solid fa-copyright'></i><br />";
+document.getElementById("setting_pane_author").innerHTML += "सौ. ने. सं. " + arabic_number_to_nepali(ns_today_year) + " | " + AD_TODAY_YEAR + " AD | वि. सं. " + arabic_number_to_nepali(bs_today_year);
+document.getElementById("setting_pane_author").innerHTML += "<br />Brihat Ratna Bajracharya";
