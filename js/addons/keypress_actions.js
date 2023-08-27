b9c857951dd37789b327e3e89dd6063808f1a21d
Brihat Ratna Bajracharya <brihat.rb@gmail.com>
2023-08-27 18:04:58 +0545

    Update keypress_actions.js

--- js/addons/keypress_actions.js
+++ js/addons/keypress_actions.js
@@ -5,18 +5,22 @@ function trigger_key_press(e) {
     e = e || window.event;
     if (e.keyCode == '37') {
         // console.log("LEFT");
+        e.preventDefault();
         previous();
     }
     else if (e.keyCode == '38') {
         // console.log("UP");
+        e.preventDefault();
         up();
     }
     else if (e.keyCode == '39') {
         // console.log("RIGHT");
+        e.preventDefault();
         next();
     }
     else if (e.keyCode == '40') {
         // console.log("DOWN");
+        e.preventDefault();
         down();
     }
     else {
